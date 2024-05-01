eb6d73eb762577a3bd4f1e15853268700b7953aa
rlacodud <rlacodud0313@naver.com>
2024-05-01 12:50:25 +0900

    chore: 기본 세팅

--- pages/index.vue
+++ pages/index.vue
@@ -1,4 +1,10 @@
 <script setup>
+// 금액 버튼 클릭 시 해당 금액에 맞는 Progress 진행률 및 진행률에 따른 Tooltip의 위치를 설정
+// Tooltip 아래에 화살표는 기본 중앙 정렬이나 좌/우측에서는 화살표가 진행률에 맞춰서 이동
+// (예: 좌측에 있을 때는 진행률에 따라 좌측으로 화살표가 이동되어야 할 수 있음)
+// Tooltip의 화살표는 항상 Tooltip 영역 범위 안에 있어야 하며 좌/우측 최소 6px의 마진이 있어야 함
+// Tooltip은 Progress bar의 영역을 벗어날 수 없음
+// 모니모 빙고 이벤트 참고
 const e = 'a'
 console.log(e)
 </script>
