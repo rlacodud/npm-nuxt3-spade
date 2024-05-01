11cc8f4f679d149116ad3e240034c5a30bb409e9
rlacodud <rlacodud0313@naver.com>
2024-05-01 20:00:55 +0900

    feat: progressbar 1차 업데이트

--- pages/index.vue
+++ pages/index.vue
@@ -4,17 +4,146 @@
 // (예: 좌측에 있을 때는 진행률에 따라 좌측으로 화살표가 이동되어야 할 수 있음)
 // Tooltip의 화살표는 항상 Tooltip 영역 범위 안에 있어야 하며 좌/우측 최소 6px의 마진이 있어야 함
 // Tooltip은 Progress bar의 영역을 벗어날 수 없음
-// 모니모 빙고 이벤트 참고
-const e = 'a'
-console.log(e)
+
+const tooltip = ref(null);
+const currentbar = ref(null);
+const progressbar = ref(null);
+let tooltipLeft = 0;
+let arrowRight = '50%';
+
+const prices = reactive([
+  { price: 0, isActive: false },
+  { price: 1000, isActive: false },
+  { price: 5000, isActive: false },
+  { price: 10000, isActive: false },
+  { price: 50000, isActive: false },
+  { price: 90000, isActive: false },
+  { price: 95000, isActive: false },
+  { price: 100000, isActive: false },
+]);
+const minPrice = Math.min(...prices.map(price => price.price));
+const maxPrice = Math.max(...prices.map(price => price.price));
+const range = maxPrice - minPrice;
+
+let activePrice = ref(prices.filter(price => price.isActive ? price.isActive : prices[0].isActive));
+
+const progressWidth = computed(() => {
+  if (range === 0) return '100%';
+  return `${((activePrice.value - minPrice) / range * 100).toFixed(2)}%`;
+});
+
+watch(() => progressWidth.value, (newValue) => {
+  const progressbarWidth = progressbar.value.clientWidth;
+  const currentbarWidth = progressbarWidth * newValue.replace('%', '') * 0.01;
+  console.log(progressbarWidth, currentbarWidth)
+  if (progressbarWidth <= currentbarWidth + tooltip.value.clientWidth / 2) {
+    console.log('넘는다')
+    tooltipLeft = progressbarWidth - (currentbarWidth + tooltip.value.clientWidth);
+    arrowRight = '6px';
+  } else if (currentbarWidth === 0) {
+    console.log('0이단')
+    arrowRight = '50%';
+  } else {
+    console.log('아니다')
+    tooltipLeft = -tooltip.value.clientWidth / 2;
+    arrowRight = '50%';
+  }
+});
+
+const handleButtonChange = (index) => {
+  prices.forEach((button, idx) => {
+    button.isActive = idx === index;
+  });
+  activePrice.value = prices[index].price;
+};
+
 </script>
 
 <template>
   <main class="contents">
-    <a href="" class="11">gkgkgkgk</a>
-    <p class="lalalal" data-popper-placement="askdaksdjk">sdkasljdlkasjdlkjasldkjsalkdjlksajdlksajlkdjsaljdlkjsalkdjaslkjslklk</p>
-    <p class="lalalal" data-popper-placement="askdaksdjk">sdkasljdlkasjdlkjasldkjsalkdjlksajdlksajlkdjsaljdlkjsalkdjaslkjslklk</p>
+    <div class="button_container">
+      <ul>
+        <li v-for="(button, index) in prices" :key="index">
+          <button type="button" :class="{ active: button.isActive}" @click="handleButtonChange(index)">{{ button.price }}</button>
+        </li>
+      </ul>
+    </div>
+    <div class="progress-bar_container">
+      <div class="progress-bar" ref="progressbar">
+        <div class="current-bar" ref="currentbar" :style="{ '--progress-width': progressWidth }">
+          <span class="tooltip_container" ref="tooltip" :style="{transform: `translateX(${tooltipLeft}px)`, '--arrow-right': arrowRight}">
+            {{ activePrice }}
+          </span>
+        </div>
+      </div>
+    </div>
   </main>
 </template>
 
-<style scoped></style>
+<style scoped lang="scss">
+.contents {
+  padding: 20px;
+  .button_container {
+  ul {
+    display: flex;
+    gap: 10px;
+    list-style: none;
+  }
+  button {
+    padding: 4px 6px;
+    color: black;
+    border: 1px solid black;
+    outline: none;
+    border-radius: 10px;
+    background-color: white;
+    &.active {
+      color: white;
+      border: none;
+      background-color: black;
+    }
+  }
+  & + .progress-bar_container {
+    margin-top: 20px;
+  }
+}
+.progress-bar {
+  position: relative;
+  width: 100%;
+  height: 10px;
+  border-radius: 10px;
+  background-color: #d8d8d8;
+  .current-bar {
+    position: absolute;
+    top: 0;
+    left: 0;
+    width: var(--progress-width);
+    height: 100%;
+    border-radius: 10px;
+    background-color: #0077ff;
+    .tooltip_container {
+      position: absolute;
+      top: calc(100% + 10px);
+      left: 100%;
+      padding: 2px 4px;
+      color: white;
+      border-radius: 4px;
+      background-color: black;
+      &::before {
+        content: '';
+        display: inline-block;
+        position: absolute;
+        bottom: 100%;
+        right: var(--arrow-right);
+        width: 0;
+        height: 0;
+        border-top: 3px solid transparent;
+        border-left: 4px solid transparent;
+        border-right: 4px solid transparent;
+        border-bottom: 6px solid black;
+        transform: translateX(50%);
+      }
+    }
+  }
+}
+}
+</style>
